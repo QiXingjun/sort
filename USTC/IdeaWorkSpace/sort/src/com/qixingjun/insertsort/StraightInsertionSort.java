7ddbd540c3a756218c7de38471d59c92b14a26be
QiXingjun <18862141550@163.com>
2017-03-02 13:11:36 +0800

    常用的8大排序算法，Java版本实现

---
+++ USTC/IdeaWorkSpace/sort/src/com/qixingjun/insertsort/StraightInsertionSort.java
@@ -0,0 +1,52 @@
+package com.qixingjun.insertsort;
+
+import java.util.Scanner;
+
+/**
+ * @Author XingJun Qi
+ * @MyBlog www.qixingjun.tech
+ * @Version 1.0.0
+ * @Date 2017/3/2
+ * @Description 直接插入排序
+ * 基本思想:
+ * 将一个记录插入到已排序好的有序表中，从而得到一个新，记录数增1的有序表。
+ * 即：先将序列的第1个记录看成是一个有序的子序列，然后从第2个记录逐个进行插入，
+ * 直至整个序列有序为止。
+ *
+ * 要点：设立哨兵，作为临时存储和判断数组边界之用。
+ *
+ * 如果碰见一个和插入元素相等的，那么插入元素把想插入的元素放在相等元素的后面。
+ * 所以，相等元素的前后顺序没有改变，从原无序序列出去的顺序就是排好序后的顺序，
+ * 所以插入排序是稳定的。
+ *
+ * 时间复杂度：O（n^2）
+ */
+public class StraightInsertionSort {
+
+    public static void insertSort(int[] arr){
+        int size = arr.length;
+        int temp = 0;
+        int j = 0;
+        for (int i = 0; i < size; i++) {
+            temp = arr[i];
+            //假如temp比前面的值小，则将前面的值后移
+            for (j = i; j>0 && temp < arr[j-1]; j--) {
+                arr[j] = arr[j-1];
+            }
+            arr[j] = temp;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int N = 10;
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        insertSort(arr);
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
---
+++ USTC/IdeaWorkSpace/sort/src/com/qixingjun/selectionsort/SimpleSelectionSort.java
@@ -0,0 +1,40 @@
+package com.qixingjun.selectionsort;
+
+import java.util.Scanner;
+
+/**
+ * @Author XingJun Qi
+ * @MyBlog www.qixingjun.tech
+ * @Version 1.0.0
+ * @Date 2017/3/2
+ * @Description
+ * 基本思想：
+ * 在要排序的一组数中，选出最小（或者最大）的一个数与第1个位置的数交换；然后在剩下的数当
+ * 中再找最小（或者最大）的与第2个位置的数交换，依次类推，直到第n-1个元素（倒数第二个数）
+ * 和第n个元素（最后一个数）比较为止。
+ *
+ * 操作方法：
+ * 第一趟，从n 个记录中找出关键码最小的记录与第一个记录交换；
+ * 第二趟，从第二个记录开始的n-1 个记录中再选出关键码最小的记录与第二个记录交换；
+ * 以此类推.....
+ * 第i 趟，则从第i 个记录开始的n-i+1 个记录中选出关键码最小的记录与第i 个记录交换，
+ * 直到整个序列按关键码有序。
+ */
+public class SimpleSelectionSort {
+    public static void selectionSort(int[] arr){
+
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        final int N = 10;
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = scanner.nextInt();
+        }
+        selectionSort(arr);
+        for (int i = 0; i < N; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
